cead27aae519b2fe1523b934eacd88d5d1bf1374
Songeun229 <cnsong0229@gmail.com>
2021-04-01 23:43:52 +0900

    테스트 커밋

---
+++ src/main/java/com/mp/p195/Project195Application.java
@@ -0,0 +1,13 @@
+package com.mp.p195;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project195Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project195Application.class, args);
+	}
+
+}
